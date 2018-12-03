58f469f8f74d9927f564acc98ddefbd392a614fb
unknown <sebastian.sobczyk95@gmail.com>
2018-12-03 19:23:27 +0100

    my first python repository

---
+++ zdrowie.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+print("Cześć powiedz mi ile ważysz:", "kg")
+waga = float(input())
+print("A powiedz mi jaki masz wzrost:", "m")
+wzrost = float(input())
+BMI = waga / (wzrost**2)
+print("Twoje BMI wynosi:", BMI)
+print("Co byś zrobił jakbyś był mały?", )
+odpowiedz = input()
+print("No co ty gadasz?")
+
